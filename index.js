310e7b33d3d7204d5180a61d1a85757d7e3600e8
KPPGid <alexbourbond@gmail.com>
2025-07-21 14:36:48 -0400

    fix: use process.env.PORT fot Glitch

--- index.js
+++ index.js
@@ -66,6 +66,8 @@ app.get("/v", (req, res) => {
   });
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server jalan di http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
